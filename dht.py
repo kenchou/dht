5edcdcb023f8ec76d648b86daa5202567269e147
RaspberryPi <kenchou77@gmail.com>
2014-04-19 19:43:54 +0800

    check temperature range

--- dht.py
+++ dht.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('sensor_type', type=int,   help='type of Sensor. 11|22|2302')
 parser.add_argument('GPIO_pin',    type=int,   help='Sensor connected to pin of GPIO')
 parser.add_argument('-t', '--try-times',    type=int, default=1, help='Try times')
-parser.add_argument('-f', '--format', help='output format. -f log will output "YYYY-MM-DD hh:mm:ss temp,humidity')
+parser.add_argument('-f', '--format', help='output format. -f log will output "YYYY-MM-DD hh:mm:ss temperature,humidity')
 parser.add_argument('-v', '--verbose', action='count',  help='verbosity')
 
 args = parser.parse_args()
@@ -51,22 +51,22 @@ if not sensor_values:
     print "sensor read timeout."
     exit(1)
 
-t, h = sensor_values
-
-temp = t
-humidity = h
+temperature, humidity = sensor_values
+if temperature < -40 or temperature > 80:
+    print "temperature out of range"
+    exit(4)
 
 now = datetime.now()
 
-data = {'datetime': now, 'temp': temp, 'humidity': humidity}
+data = {'datetime': now, 'temperature': temperature, 'humidity': humidity}
 
 if args.format == 'log':
-    output_format = '{datetime:%Y-%m-%d %H:%M:%S} {temp:.1f},{humidity:.1f}'
+    output_format = '{datetime:%Y-%m-%d %H:%M:%S} {temperature:.1f},{humidity:.1f}'
 elif args.format == 'simple':
-    output_format = '{temp:.1f},{humidity:.1f}'
+    output_format = '{temperature:.1f},{humidity:.1f}'
 elif args.format:
     output_format = args.format
 else:
-    output_format = '温度: {temp:.1f}°C, 相对湿度: {humidity:.1f}%'
+    output_format = '温度: {temperature:.1f}°C, 相对湿度: {humidity:.1f}%'
 
 print output_format.format(**data)
